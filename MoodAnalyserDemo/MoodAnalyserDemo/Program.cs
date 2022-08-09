3d21b6f555545149cce70d300ce5b73e0e06bd04
senpooja <poojasen744055@gmail.com>
2022-08-09 23:51:38 +0530

    Added Wellcome Message

---
+++ MoodAnalyserDemo/MoodAnalyserDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MoodAnalyserProblems
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Problem Program");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MoodAnalyserDemo/MoodAnalyserTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MoodAnalyserTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
